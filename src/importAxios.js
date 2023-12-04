d91c1fd09c4775f2f0bfab9bfc5c50130e1f34d5
AndreaBartoloni <andrea.bartoloni00@gmail.com>
2023-12-04 16:43:43 +0100

    commit

--- src/importAxios.js
+++ src/importAxios.js
@@ -3,10 +3,14 @@ import axios from 'axios';
 const options = {
   method: 'GET',
   url: 'https://api.themoviedb.org/3/search/movie',
-  params: {include_adult: 'false', language: 'en-US', page: '1'},
+  params: {
+    query: encodeURI(this.quelDato),
+    include_adult: 'false', 
+    language: 'en-US', 
+    page: '1'
+},
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YTM1MTNkYTk2NjYyM2RlYWFjYTM4MzNiOGVkMjc5NSIsInN1YiI6IjY1NmRkZWQzMDVhNTMzMDBhZDU2Y2Y3MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R2BhiAfdojmFUGgmmDa9xoGraWsUcUc6Qidkfq-4t-U'
   }
 };
 
